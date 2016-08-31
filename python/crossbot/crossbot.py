464a60c452c7df1c9a1ec098bec3b235fa281db5
Harriet Dashnow <h.dashnow@gmail.com>
2016-08-31 16:41:30 +1000

    change crossbot regex to allow for channel name, still is having a weird error for regex groups, but works anyway

--- python/crossbot/crossbot.py
+++ python/crossbot/crossbot.py
@@ -7,17 +7,19 @@ import re
 sc = SlackClient(API_TOKEN)
 
 @listen_to('<#.*>')
-def single_cell(message):
+def cross_post(message):
     """Listen for mention of any channel. When a channel is mentioned,
     post contents of message to that channel.
     """
     message_text = message.body['text']
-    re_result = re.search('<#([a-zA-Z0-9]*)>', message_text)
+    print(message.body)
+    re_result = re.search('<#([a-zA-Z0-9]*).*>', message_text)
+
     channel = re_result.group(1)
 
     sc.api_call(
-            "chat.postMessage", channel=channel, text=message_text,
-            username='crossbot', icon_emoji=':rage:'
+        "chat.postMessage", channel=channel, text=message_text,
+        username='crossbot', icon_emoji=':rage:'
     )
 
 def main():
